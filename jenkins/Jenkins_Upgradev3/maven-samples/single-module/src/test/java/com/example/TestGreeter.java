2c3a6447cde9fc850285f2a8d4436bfd5acb998b
AposLaz <aplazidis@gmail.com>
2024-04-21 13:56:51 +0300

    fix: remove custom test case

--- jenkins/Jenkins_Upgradev3/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ jenkins/Jenkins_Upgradev3/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -28,12 +28,6 @@ public class TestGreeter {
   }
 
   @Test
-  public void greetShouldBePassEver() {
-    assertTrue(true);
-  }
-  
-
-  @Test
   public void greetShouldIncludeGreetingPhrase() {
     String someone = "World";
 
